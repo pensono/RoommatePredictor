6852878b6772c474ccf9285cf62e9199b7121941
Ethan Shea <ethan.shea1@gmail.com>
2019-08-18 13:27:14 -0700

    Additional site tweaks

--- app/static/predictor.js
+++ app/static/predictor.js
@@ -1,23 +1,30 @@
 $(function () {
-    $("#submit-prediction").click(function () {
-        var phrase = $("#phrase-input").val();
-        $.ajax({
-            url: "/api/predict/" + phrase
-        })
-            .then(JSON.parse)
-            .done(function (data) {
-                let people = Object.keys(data)
-                    .sort(function(a,b){return data[b]-data[a]})
-                    .map(name => ({
-                        name: name,
-                        percent: (data[name] * 100).toFixed(2)
-                    }));
-
-                $("#prediction-result").empty();
-                $("#prediction-template").tmpl(people[0]).appendTo('#prediction-result');
-
-                $("#prediction-results").empty();
-                $("#percent-template").tmpl(people).appendTo('#prediction-results');
-            });
+    $("#submit-prediction").click(refreshPrediction);
+    $("#phrase-input").on('keypress', function(e) {
+        if (e.which === 13) {
+            refreshPrediction();
+        }
     });
 });
+
+function refreshPrediction() {
+    let phrase = $("#phrase-input").val();
+    $.ajax({
+        url: "/api/predict/" + phrase
+    })
+        .then(JSON.parse)
+        .done(function (data) {
+            let people = Object.keys(data)
+                .sort(function(a,b){return data[b]-data[a]})
+                .map(name => ({
+                    name: name,
+                    percent: (data[name] * 100).toFixed(2)
+                }));
+
+            $("#prediction-result").empty();
+            $("#prediction-template").tmpl(people[0]).appendTo('#prediction-result');
+
+            $("#prediction-results").empty();
+            $("#percent-template").tmpl(people).appendTo('#prediction-results');
+        });
+}
